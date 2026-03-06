393866abd903191086c577a0cd64eb864aa82f0e
GJ Bos <bosmeel@gmail.com>
2026-03-06 19:25:58 +0100

    disable prerender events pages

--- app/what-happened-on/[date]/page.tsx
+++ app/what-happened-on/[date]/page.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 type PageProps = {
   params: Promise<{
     date: string;
@@ -26,20 +28,6 @@ function formatSlug(slug: string) {
   const [month, day] = slug.split("-");
   const monthName = month.charAt(0).toUpperCase() + month.slice(1);
   return `${monthName} ${day}`;
-}
-
-export function generateStaticParams() {
-  const routes: { date: string }[] = [];
-
-  months.forEach((month) => {
-    for (let d = 1; d <= month.days; d++) {
-      routes.push({
-        date: `${month.name}-${d}`,
-      });
-    }
-  });
-
-  return routes;
 }
 
 export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
