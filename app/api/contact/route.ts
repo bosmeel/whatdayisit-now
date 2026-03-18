f7f980cc70c20c76c57ca26c0c7218ef2b20fd6e
GJ Bos <bosmeel@gmail.com>
2026-03-18 05:11:03 +0100

    Fix contact API

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,25 +4,21 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
-
-  const { email, message } = await req.json();
-
   try {
+    const { email, message } = await req.json();
 
-    await resend.emails.send({
-      from: "WhatDayIsIt <noreply@whatdayisit.now>",
+    const data = await resend.emails.send({
+      from: "WhatDayIsIt <onboarding@resend.dev>",
       to: "contact@whatdayisit.now",
-      subject: "Website message",
-      reply_to: email,
-      text: message
+      subject: "New message from website",
+      reply_to: email || undefined,
+      text: message,
     });
 
-    return NextResponse.json({ success: true });
+    return NextResponse.json({ success: true, data });
 
   } catch (error) {
-
+    console.error(error);
     return NextResponse.json({ success: false });
-
   }
-
 }
